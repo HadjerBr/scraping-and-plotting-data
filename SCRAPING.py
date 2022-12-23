e5742761e601b9a3d2383babb96cf36cdcb69b00
Hadjer Brioua <72814758+HadjerBr@users.noreply.github.com>
2022-12-23 12:11:28 +0300

    Add files via upload

---
+++ SCRAPING.py
@@ -0,0 +1,58 @@
+# scrapping indeed.com
+# getting salary data for bilgisayar muhendisi, veri bilimci, yazilim muhendisi, and mobil yazilim muhendisi salaries , the most hired fields and sektör  in turkey  DONE
+# storing the data in csv file DONE
+# plotting the data using matplotlib DONE
+
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+def scraping():
+    file = open('files/jobs_salaries.csv', 'w', newline="")
+    writer = csv.writer(file)
+    writer.writerow(["is", "ortalama maas", "en yuksek maas", "en dusuk maas"])
+
+    result = requests.get("https://www.kariyer.net/pozisyonlar").content
+    soup = BeautifulSoup(result, "lxml")
+    jobs = soup.find_all("a")
+
+
+    for job in jobs:
+        row = []
+        job_name = job.text
+        if job_name == "Bilgisayar Mühendisi" or job_name == "Veri Bilimci" or job_name == "Yazılım Mühendisi" or job_name == "Mobil Yazılım Mühendisi":
+            row += [job_name]
+            link = job["href"]
+            result1 = requests.get("https://www.kariyer.net" + link).content
+            soup1 = BeautifulSoup(result1, "lxml")
+            ortalama_salary = soup1.find("div", class_ = "pg-salary-box-value").text
+            row += [ortalama_salary]
+            a = soup1.find("div", class_ = "pg-salary-box double")
+            b = a.find("div", class_ = "left")
+            c = a.find("div", class_ = "right")
+            en_yuksek = c.find("div", class_ = "pg-salary-box-value").text
+            row += [en_yuksek]
+            en_dusuk = b.find("div", class_ = "pg-salary-box-value").text
+            row += [en_dusuk]
+        
+            file1 = open(f'files/{job_name}_most_hired_fields.csv', 'w', newline="")
+            writer1 = csv.writer(file1)
+            writer1.writerow(["field", "ortalama"])
+            fields = []
+            averages = []
+            bolumler = soup1.find_all("div", class_= "pg-progress-row")
+            for bolum in bolumler:
+                bolum = bolum.text.strip().split("\n")
+                fields += [bolum[0]]
+                averages += [bolum[1]]
+            for i in range(5):
+                fields.pop(-1)
+                averages.pop(-1)
+            
+            for i in range(len(fields)):
+                row1 = []
+                row1+= [fields[i], averages[i]]
+                writer1.writerow(row1)
+            writer.writerow(row)
+
+    file.close()
---
+++ main.py
@@ -0,0 +1,94 @@
+# plotting the data using matplotlib DONE
+
+import pandas as pd
+from matplotlib import pyplot as plt
+import csv
+import SCRAPING
+
+
+SCRAPING.scraping()
+
+a = []
+b = []
+
+with open('files/jobs_salaries.csv','r') as file:
+    plots = csv.reader(file, delimiter = ',')
+    next(plots)
+    for row in plots:
+        a.append(row[0])
+        b.append(int(row[1].replace(".", "")))
+file.close()
+
+plt.bar(a, b, width = 0.72, label = "Ortalama Maas")
+plt.xlabel('Isler')
+plt.ylabel('Yillik Ortalama Maaslar(TL)')
+plt.title('Bilgisayar Bilimleri Islerin Ortalama Maaslari')
+plt.legend()
+# plt.show()
+
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+
+
+x = []
+y = []
+
+x1 = []
+y1 = []
+
+x2 = []
+y2 = []
+
+x3 = []
+y3 = []
+
+
+with open('files/Bilgisayar Mühendisi_most_hired_fields.csv','r') as file:
+    plots = csv.reader(file, delimiter = ',')
+    next(plots)
+    for row in plots:
+        x.append(row[0])
+        y.append(int(float(row[1].replace(',', '').replace('%', ''))))
+file.close()
+
+
+with open('files/Mobil Yazılım Mühendisi_most_hired_fields.csv','r') as file:
+    plots = csv.reader(file, delimiter = ',')
+    next(plots)
+    for row in plots:
+        x1.append(row[0])
+        y1.append(int(float(row[1].replace(',', '').replace('%', ''))))
+file.close()
+
+with open('files/Veri Bilimci_most_hired_fields.csv','r') as file:
+    plots = csv.reader(file, delimiter = ',')
+    next(plots)
+    for row in plots:
+        x2.append(row[0])
+        y2.append(int(float(row[1].replace(',', '').replace('%', ''))))
+file.close()
+
+with open('files/Yazılım Mühendisi_most_hired_fields.csv','r') as file:
+    plots = csv.reader(file, delimiter = ',')
+    next(plots)
+    for row in plots:
+        x3.append(row[0])
+        y3.append(int(float(row[1].replace(',', '').replace('%', ''))))
+file.close()
+
+
+colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#8c564b"]
+patches, texts = ax1.pie(y, colors=colors, shadow=True, startangle=90)
+patches1, texts1 = ax2.pie(y1, colors=colors, shadow=True, startangle=90)
+patches2, texts2 = ax3.pie(y2, colors=colors, shadow=True, startangle=90)
+patches3, texts3 = ax4.pie(y3, colors=colors, shadow=True, startangle=90)
+ax1.set_title("Bilgisayar Mühendisi pozisyonunda most hired 5 fields")
+ax1.legend(patches, x, loc="best")
+ax2.set_title("Mobil Yazılım Mühendisi pozisyonunda most hired 5 fields")
+ax2.legend(patches1, x1, loc="best")
+ax3.set_title("Veri Bilimci pozisyonunda most hired 5 fields")
+ax3.legend(patches2, x2, loc="best")
+ax4.set_title("Yazılım Mühendisi pozisyonunda most hired 5 fields")
+ax4.legend(patches3, x3, loc="best")
+plt.show()
+plt.show()
+  
